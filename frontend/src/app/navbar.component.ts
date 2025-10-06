ca84642a1739c452a970768f2e9d6aa55be26e6d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-07 00:27:49 +0200

    navbar component

--- frontend/src/app/navbar.component.ts
+++ frontend/src/app/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class AppComponent {
-  title = 'Project';
+export class NavbarComponent {
+  private readonly authService = inject(AuthService);
+  auth = computed( () => this.authService.isAuthenticated());
+  go(path: string) {location.href = path;}
+  logout() { this.authService.logout(); this.go('/login');}
 }
