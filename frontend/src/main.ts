f42015c2c8ecce6266b1f7b19477597e7de7bee8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-07 00:21:02 +0200

    some styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/navbar.component';
 import { provideRouter } from '@angular/router';
 
 bootstrapApplication(NavbarComponent, {
