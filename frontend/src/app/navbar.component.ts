52c48a6f5d94a24ff69b18669d7eb8d99e149abd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-07 23:16:52 +0200

    auth guards

--- frontend/src/app/navbar.component.ts
+++ frontend/src/app/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
