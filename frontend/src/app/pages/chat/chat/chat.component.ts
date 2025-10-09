ee86293e9e9e6a41eb100285273460076a627f2c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-09 23:08:33 +0200

    setup chat component

---
+++ frontend/src/app/pages/chat/chat/chat.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css'
+})
+export class ChatComponent {
+
+}
