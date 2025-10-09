10d70978c754a891e8bea8201f5590183e4e193c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-09 23:05:14 +0200

    make login component

--- frontend/src/app/pages/login/login/login.component.ts
+++ frontend/src/app/pages/login/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,5 +12,20 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  submit(){
+    const {email, password} = this.form.value;
+    if (this.auth.login(email!, password!)) {
+      this.router.navigateByUrl('/dashboard');
+    }
+  }
 
 }
