0728744c8b279758849e480dde0c8e9e6c0dc0be
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-09 22:54:49 +0200

    make register form

--- frontend/src/app/pages/register/register/register.component.ts
+++ frontend/src/app/pages/register/register/register.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,5 +12,20 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  submit(){
+    const {email, password} = this.form.value;
+    if (this.auth.register(email!, password!)){
+      this.router.navigateByUrl('/dashboard');
+    }
+  }
 
 }
