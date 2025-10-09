9a135a3943908035fa85ae592dd9a38ce7ee233d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-09 22:46:40 +0200

    register form

---
+++ frontend/src/app/pages/register/register/register.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
